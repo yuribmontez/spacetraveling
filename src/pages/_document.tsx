227cab0955b005d2ba85ee05d352d984159c024e
yuribmontez <ybmontez@gmail.com>
2021-09-19 21:14:49 -0300

    parcial: app favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
 
-          <link rel="shortcut icon" href="/logo.svg" type="image/svg" />
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
         <body>
           <Main />
